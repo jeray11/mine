9ff06b72bc2ede93601931847321eb9b0e542079
jeray11 <jeray11@163.com>
2016-06-06 17:32:43 +0800

    second

---
+++ mine.data/Mapping/Forums/ForumGroupMap.cs
@@ -0,0 +1,20 @@
+﻿using mine.core.Domain.Forums;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine.data.Mapping.Forums
+{
+    public class ForumGroupMap : EntityTypeConfiguration<ForumGroup>
+    {
+        public ForumGroupMap()
+        {
+            this.ToTable("Forums_Group");
+            this.HasKey(fg => fg.Id);
+            this.Property(fg => fg.Name).IsRequired().HasMaxLength(200);
+        }
+    }
+}
---
+++ mine.data/Mapping/Forums/ForumMap.cs
@@ -0,0 +1,24 @@
+﻿using mine.core.Domain.Forums;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine.data.Mapping.Forums
+{
+    public class ForumMap : EntityTypeConfiguration<Forum>
+    {
+        public ForumMap() 
+        {
+            this.ToTable("Forums_Forum");
+            this.HasKey(item=>item.Id);
+            this.Property(item=>item.Name).IsRequired().HasMaxLength(200);
+
+            this.HasRequired(item => item.ForumGroup)
+                .WithMany(fg => fg.Forums)
+                .HasForeignKey(f=>f.ForumGroupId);
+        }
+    }
+}
