26edea087cbb7c09c23a04f21f13b17a5ea57d3c
jeray11 <116207661@qq.com>
2016-06-12 10:24:06 +0800

    fdsfsfsdfsf

---
+++ mine.core/Infrastructure/ContainerManager.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine.core.Infrastructure
+{
+    public class ContainerManager
+    {
+        private readonly IContainer _container;
+        public ContainerManager(IContainer container)
+        {
+            this._container = container;
+        }
+        public virtual IContainer Container
+        {
+            get
+            {
+                return _container;
+            }
+        }
+
+        public virtual T Resolve<T>(string key = "", ILifetimeScope scope = null) where T : class
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            if (string.IsNullOrEmpty(key))
+            {
+                return scope.Resolve<T>();
+            }
+            return scope.ResolveKeyed<T>(key);
+        }
+    }
+}
---
+++ mine.core/Infrastructure/MineEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine.core.Infrastructure
+{
+    public class MineEngine : IEngine
+    {
+        public ContainerManager ContainerManager
+        {
+            get;
+        }
+
+        public void Initialize(NopConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ResolveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
