8f604cb212d8d575ca5f211797b5bd44d936a72e
jeray11 <jeray11@163.com>
2016-06-12 17:25:15 +0800

    dsadadsad

---
+++ mine.core/Infrastructure/EngineContext.cs
@@ -0,0 +1,49 @@
+﻿using mine.core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mine.core.Infrastructure
+{
+    public class EngineContext
+    {
+        /// <summary>
+        /// Creates a factory instance and adds a http application injecting facility.
+        /// </summary>
+        /// <param name="config">Config</param>
+        /// <returns>New engine instance</returns>
+        protected static IEngine CreateEngineInstance(MineConfig config)
+        {
+            if (config != null && !string.IsNullOrEmpty(config.EngineType))
+            {
+                var engineType = Type.GetType(config.EngineType);
+                if (engineType == null)
+                    throw new ConfigurationErrorsException("The type '" + config.EngineType + "' could not be found. Please check the configuration at /configuration/nop/engine[@engineType] or check for missing assemblies.");
+                if (!typeof(IEngine).IsAssignableFrom(engineType))
+                    throw new ConfigurationErrorsException("The type '" + engineType + "' doesn't implement 'Nop.Core.Infrastructure.IEngine' and cannot be configured in /configuration/nop/engine[@engineType] for that purpose.");
+                return Activator.CreateInstance(engineType) as IEngine;
+            }
+
+            return new NopEngine();
+        }
+        /// <summary>
+        /// Initializes a static instance of the Nop factory.
+        /// </summary>
+        /// <param name="forceRecreate">Creates a new factory instance even though the factory has been previously initialized.</param>
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static IEngine Initialize(bool forceRecreate)
+        {
+            if (Singleton<IEngine>.Instance == null || forceRecreate)
+            {
+                var config = ConfigurationManager.GetSection("MineConfig") as MineConfig;
+                Singleton<IEngine>.Instance = CreateEngineInstance(config);
+                Singleton<IEngine>.Instance.Initialize(config);
+            }
+            return Singleton<IEngine>.Instance;
+        }
+    }
+}
