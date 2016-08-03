f1e274f6bcc3751756f720d7ebb518feabf581ee
jeray11 <jeray11@163.com>
2016-08-03 15:22:29 +0800

    123

--- mine.web/Controllers/CommonController.cs
+++ mine.web/Controllers/CommonController.cs
@@ -1,11 +1,15 @@
 ﻿using mine.core;
 using mine.core.Caching;
+using mine.services.Directory;
 using mine.web.Infrastructure;
+using mine.web.Models.Common;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mine.services.Localization;
 
 namespace mine.web.Controllers
 {
@@ -14,11 +18,13 @@ namespace mine.web.Controllers
         private readonly ICacheManager _cacheManager;
         private readonly IWorkContext _workContext;
         private readonly IStoreContext _storeContext;
-        public CommonController(ICacheManager cacheManager,IWorkContext workContext, IStoreContext storeContext) 
+        private readonly ICurrencyService _currencyService;
+        public CommonController(ICacheManager cacheManager,IWorkContext workContext, IStoreContext storeContext,ICurrencyService currencyService)
         {
             this._cacheManager = cacheManager;
             this._workContext = workContext;
             this._storeContext = storeContext;
+            this._currencyService = currencyService;
         }
         //
         // GET: /Common/
@@ -26,7 +32,24 @@ namespace mine.web.Controllers
         public ActionResult CurrencySelector()
         {
             var availableCurrencies = _cacheManager.Get(string.Format(ModelCacheEventConsumer.AVAILABLE_CURRENCIES_MODEL_KEY,_workContext.WorkingLanguage.Id,_storeContext.CurrentStore.Id),()=> {
+                var query = _currencyService.GetAllCurrencies(storeId: _storeContext.CurrentStore.Id)
+                    .Select(c => {
+                        var currencySymbol = "";
+                        if (!string.IsNullOrEmpty(c.DisplayLocale))
+                            currencySymbol = new RegionInfo(c.DisplayLocale).CurrencySymbol;
+                        else
+                            currencySymbol = c.CurrencyCode;
+                        //model
+                        var currencyModel = new CurrencyModel
+                        {
+                            Id = c.Id,
+                            Name = c.GetLocalized(y => y.Name),
+                            CurrencySymbol = currencySymbol
+                        };
+                        return currencyModel;
+                    });
 
+                return query;
             });
             return View();
         }
---
+++ mine.web/Models/Common/CurrencyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mine.web.Models.Common
+{
+    public class CurrencyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CurrencySymbol { get; set; }
+    }
+}
